362e2985eb4f5607759727d2f473f3b59f4ed9a5
Jeremy Stashewsky <jeremy@goinstant.com>
2013-11-08 15:19:37 -0800

    Node 0.11 seems to return uninitialized HMACs!

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+if (process.version.match(/^v0\.11\./)) {
+  console.error('goinstant-auth is not safe to use with node v0.11!');
+  console.error('node v0.11.3 appears to have a bug where it returns '+
+                'uninitialized memory for HMAC functions!');
+  throw new Error('goinstant-auth is not yet safe to use in node v0.11!');
+}
+
 module.exports.Signer = require('./lib/signer');
